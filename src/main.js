66243abc1404115e209d68a30e42fec448a7b9c3
Oskar Artes <o.artes@wnm.de>
2024-10-25 14:00:05 +0200

    bg color Icon template component logo image inside assets

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Router/router.js'
-import './style.scss'
+import './assets/style.scss'
 
 
 const app = createApp(App)
