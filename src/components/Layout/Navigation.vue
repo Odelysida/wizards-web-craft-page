3afc93bac0ccee791b3f1e74155a5747654b077d
Louis Meyer <louis.meyer@securepoint.de>
2024-12-06 12:16:45 +0100

    Add icon navigation and favicon

--- src/components/Layout/Navigation.vue
+++ src/components/Layout/Navigation.vue
@@ -10,12 +10,13 @@ const currentRoute = useRoute();
     <div class="container-xxl mw-100">
       <ul class="nav justify-content-start w-350px">
         <li class="nav-item d-flex">
-          <img src="../../assets/logo.png" class="text-black h-40px" alt="logo"/>
-          <h1 class="fs-4 w-100 mt-2 heading" >Baller Los Brettspiele</h1>
+          <RouterLink class="d-flex align-items-center" :to="{name: 'home'}">
+            <img src="../../assets/logo.png" class="text-black h-40px" alt="logo"/>
+          </RouterLink>
+          <h1 class="fs-4 w-100 mt-2 heading">Baller Los Brettspiele</h1>
         </li>
       </ul>
       <ul class="nav nav-fill d-flex flex-row justify-content-start">
-
         <li class="nav-item" :class="{'nav-item--active': currentRoute.matched.some(({name}) => name === 'home')}">
           <RouterLink class="nav-link" :to="{name: 'home'}" active-class="active">
             <HomeIcon class="text-black h-40px" />
@@ -35,6 +36,7 @@ const currentRoute = useRoute();
     </div>
   </nav>
 </template>
+
 
 <style scoped>
 .h-40px {
