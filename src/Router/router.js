ce9a86fceab6a1f398b121bb20771d2ef1dd47ca
Louis Meyer <louis.meyer@securepoint.de>
2025-02-28 12:34:21 +0100

    Add scrollable effect when switching tabs

--- src/Router/router.js
+++ src/Router/router.js
@@ -7,5 +7,12 @@ const router = createRouter({
         path: '/',
         children: publicRoutes,
     }],
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0, left: 0 };
+        }
+    }
 });
 export default router;
